4f199010f9040a377b6242afd26eab16e514e55a
Lagado <36730990+Eduardokpv@users.noreply.github.com>
2019-11-14 12:19:15 -0300

    change17

--- views/home.php
+++ views/home.php
@@ -2,6 +2,10 @@
     <div class="col-sm-4">
         <?php $this->LoadView('product_item', array()); ?>
     </div>
-    <div class="col-sm-4">...</div>
-    <div class="col-sm-4">...</div>
+    <div class="col-sm-4">
+        <?php $this->LoadView('product_item', array()); ?>
+    </div>
+    <div class="col-sm-4">
+        <?php $this->LoadView('product_item', array()); ?>
+    </div>
 </div>
--- views/product_item.php
+++ views/product_item.php
@@ -1,5 +1,21 @@
+<div class="clear:both;"></div>
 <div class="product_item">
-    <div class="product_image">
-        <img src="Media/Products/1.jpg" width="100%">
-    </div>
+    <a href="">
+        <div class="product_tags">
+            <div class="product_tag product_tag_red">SALE</div>
+            <div class="product_tag product_tag_green">BESTSELLER</div>
+            <div class="product_tag product_tag_blue">NEW!</div>
+        </div>
+        <div class="product_image">
+            <img src="Media/Products/2.jpg" width="100%">
+        </div>
+        <div class="product_name">Sapato X</div>
+        <div class="product_brand">Marca Y</div>
+        <div>....</div>
+
+        <div class="product_price_from">R$ xxx</div>
+        <div class="product_price">R$ yyy</div>
+        <div>....</div>
+        <div class="clear:both;"></div>
+    </a>
 </div>
